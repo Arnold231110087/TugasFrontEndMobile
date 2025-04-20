6b2fd6c136cc231a0df7c49c3e6ff907dd63a190
valerio231110232 <valerio.liuz@students.mikroskil.ac.id>
2025-04-20 10:52:26 +0700

    Membuat row pertama di halaman upload

--- lib/upload_page.dart
+++ lib/upload_page.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
 
 class UploadPage extends StatelessWidget {
+  final Color primaryColor = Color(0xFF1E3A8A);
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Halaman Unggah Postingan"));
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: primaryColor,
+        title: Text("Unggahan Baru",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
+        leading: IconButton(onPressed: (){Navigator.pop(context);}, icon: Icon(Icons.arrow_back),),
+      ),
+      body:Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                CircleAvatar(backgroundImage: AssetImage("assets/1.png"),),
+                SizedBox(width: 30,),
+                Text("Valerio Liuz Kienata",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),)
+              ],
+            )
+          ],
+        ),
+        ),
+        
+    );
   }
 }
